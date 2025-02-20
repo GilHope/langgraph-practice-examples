5f280c3d16c8e0b128419e5463732ffe8d592e06
Gil <ghopecs@gmail.com>
2025-02-20 15:01:47 -0500

    brb

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ load_dotenv()
 
 from langchain_core.messages import BaseMessage, HumanMessage
 from langgraph.graph import END, MessageGraph
+
+from chains import generate_chain, reflect_chain
+
+
+REFLECT = "reflect"
+GENERATE = "generate"
+
+
+def generation_node(state: Sequence[BaseMessage]):
+    return generate_chain.invoke({"messages": state})
+
+def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
+    res = reflect_chain.invoke({"messages": messages})
+    return [HumanMessage(content=res.content)]
