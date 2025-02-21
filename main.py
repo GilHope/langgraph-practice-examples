d92960c615f1d138c10ab57b2ac75801c68c5b82
Gil <ghopecs@gmail.com>
2025-02-21 10:09:12 -0500

    add notes

--- main.py
+++ main.py
@@ -17,19 +17,30 @@ REFLECT = "reflect"
 GENERATE = "generate"
 # These are the keys for the nodes in our LangGraph graph.
 
+
 # Nodes
 def generation_node(state: Sequence[BaseMessage]):
     return generate_chain.invoke({"messages": state})
+# The 'generation_node' function will receive as an input the state.
+# The 'state' is the list of messages that have been passed through the graph so far.
+# The function will then run the 'generate_chain' chain, which will generate a response based on the state.
 
 def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
     res = reflect_chain.invoke({"messages": messages})
     return [HumanMessage(content=res.content)]
+# The 'reflection_node' function will also receive the sequence of messages.
+# It will then invoke the 'reflect_chain' but instead will respond with a list of messages in the format of a HumanMessage.
 
 
-# Graph
+
+# Initialize Graph
 builder = MessageGraph()
+
+# Define the nodes in the graph
 builder.add_node(GENERATE, generation_node)
 builder.add_node(REFLECT, reflection_node)
+
+# Tell the graph which node to start from (entry point)
 builder.set_entry_point(GENERATE)
 
 
